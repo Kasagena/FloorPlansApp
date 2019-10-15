76b0a522924554f3c1a4b699b1d4c17e2e038462
Kasagena <Kasagena@users.noreply.github.com>
2019-10-16 06:56:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/floorplans/hallway.java
@@ -0,0 +1,14 @@
+package com.example.floorplans;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hallway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallway);
+    }
+}
